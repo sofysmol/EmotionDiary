512dde46ff8aa7a461c8a54548b544851d208a7a
sofysmol <sov-95@mail.ru>
2016-04-17 02:06:43 +0300

    implement PersonalAnalis

--- EmotionDiary/app/src/main/java/com/sov/sofysmo/emotiondiary/Activity/PersonalAnalisesActivity.java
+++ EmotionDiary/app/src/main/java/com/sov/sofysmo/emotiondiary/Activity/PersonalAnalisesActivity.java
@@ -8,7 +8,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.ButtonBarLayout;
+import android.os.Handler;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,11 +16,15 @@ import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.SeekBar;
+import com.ibm.watson.developer_cloud.personality_insights.v2.model.Profile;
 
+import com.ibm.watson.developer_cloud.personality_insights.v2.model.Trait;
 import com.sov.sofysmo.emotiondiary.R;
+import com.sov.sofysmo.emotiondiary.Utils.PersonalityConnect;
 
 import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
+
+import java.util.List;
 
 public class PersonalAnalisesActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -36,35 +40,8 @@ public class PersonalAnalisesActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-        DiscreteSeekBar seekBar=(DiscreteSeekBar) findViewById(R.id.agreeabl);
-        seekBar.setProgress(50);
-        ((Button)findViewById(R.id.agreeabl_button)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                LinearLayout ll=(LinearLayout) findViewById(R.id.agreeabl_plus);
-                int height=ll.getLayoutParams().height;
-                if(height!=0)
-                {
-                    height=0;
-                    RotateAnimation rotate = new RotateAnimation(180.0f, 0.0f,
-                            Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-                    rotate.setFillAfter(true);
-
-                    view.startAnimation(rotate);
-                }
-                        else
-                {
-                    RotateAnimation rotate = new RotateAnimation(0.0f, 180.0f,
-                            Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-                    rotate.setFillAfter(true);
-
-                    view.startAnimation(rotate);
-                    height=1000;
-                }
-                ll.getLayoutParams().height=height;
-                ll.requestLayout();
-            }
-        });
+        initSlider();
+        initButton();
 
         //LinearLayout ll=(LinearLayout) findViewById(R.id.agreeabl_plus);
         //ll.getLayoutParams().height=1300;
@@ -101,4 +78,130 @@ public class PersonalAnalisesActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+    private void showMoreInformation(View view, LinearLayout ll)
+    {
+        int height=ll.getLayoutParams().height;
+        if(height!=0)
+        {
+            height=0;
+            RotateAnimation rotate = new RotateAnimation(180.0f, 0.0f,
+                    Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+            rotate.setFillAfter(true);
+
+            view.startAnimation(rotate);
+        }
+        else
+        {
+            RotateAnimation rotate = new RotateAnimation(0.0f, 180.0f,
+                    Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+            rotate.setFillAfter(true);
+
+            view.startAnimation(rotate);
+            height=1000;
+        }
+        ll.getLayoutParams().height=height;
+        ll.requestLayout();
+    }
+    private void initSlider()
+    {
+        PersonalityConnect personality;
+        Handler handlerPersonality= new Handler() {  // посрденик между потоками(он нам понадобиться для того, чтобы передать данные из другого потока в поток UI)
+            public void handleMessage(android.os.Message msg) {
+                Profile profile = (Profile) msg.obj; //Получаем profile от Watson
+                List<Trait> data=profile.getTree().getChildren().get(0).getChildren();
+                initOpeninness(data.get(0).getChildren().get(0));
+                initConscientiousness(data.get(0).getChildren().get(1));
+                initExtraversion(data.get(0).getChildren().get(2));
+                initAgreeabl(data.get(0).getChildren().get(3));
+
+                }
+        };;
+        personality = new PersonalityConnect("47c494e1-40b4-4f7b-9dfc-aa6d744a7480", "KMCFyQeEK4BN");
+        personality.setHandler(handlerPersonality);
+        String myProfile = "Call me Ishmael. Some years ago-never mind how long precisely-having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people's hats off-then, I account it high time to get to sea as soon as I can.";
+        personality.runRequestServiceSpeechToText(myProfile);
+
+
+    }
+
+    private void initButton()
+    {
+        ((Button) findViewById(R.id.agreeabl_button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LinearLayout ll3 = (LinearLayout) findViewById(R.id.agreeabl_plus);
+                showMoreInformation(view, ll3);
+            }
+        });
+        ((Button) findViewById(R.id.intr_extr_button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LinearLayout ll3 = (LinearLayout) findViewById(R.id.intr_extr_plus);
+                showMoreInformation(view, ll3);
+            }
+        });
+        ((Button) findViewById(R.id.emotional_range_button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LinearLayout ll3 = (LinearLayout) findViewById(R.id.emotional_range_plus);
+                showMoreInformation(view, ll3);
+            }
+        });
+        ((Button) findViewById(R.id.openness_button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LinearLayout ll3 = (LinearLayout) findViewById(R.id.openness_plus);
+                showMoreInformation(view, ll3);
+            }
+        });
+        ((Button) findViewById(R.id.conscientiousness_button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LinearLayout ll3 = (LinearLayout) findViewById(R.id.conscientiousness_plus);
+                showMoreInformation(view, ll3);
+            }
+        });
+    }
+    private void initOpeninness(Trait openinness)
+    {
+        ((DiscreteSeekBar) findViewById(R.id.openness)).setProgress((int)(openinness.getPercentage()*100));
+        List<Trait> children=openinness.getChildren();
+        ((DiscreteSeekBar) findViewById(R.id.openness_adventurousness)).setProgress((int)(children.get(0).getPercentage()*100));
+        ((DiscreteSeekBar) findViewById(R.id.openness_artistic_interests)).setProgress((int)(children.get(1).getPercentage()*100));
+        ((DiscreteSeekBar) findViewById(R.id.openness_emotionality)).setProgress((int)(children.get(3).getPercentage()*100));
+        ((DiscreteSeekBar) findViewById(R.id.openness_imagination)).setProgress((int)(children.get(4).getPercentage()*100));
+        ((DiscreteSeekBar) findViewById(R.id.openness_intellect)).setProgress((int)(children.get(5).getPercentage()*100));
+        ((DiscreteSeekBar) findViewById(R.id.openness_authority_challenging)).setProgress((int)(children.get(6).getPercentage()*100));
+    }
+    private void initConscientiousness(Trait openinness)
+    {
+        ((DiscreteSeekBar) findViewById(R.id.conscientiousness)).setProgress((int)(openinness.getPercentage()*100));
+        List<Trait> children=openinness.getChildren();
+        ((DiscreteSeekBar) findViewById(R.id.conscientiousness_achievement_striving)).setProgress((int)(children.get(0).getPercentage()*100));
+        ((DiscreteSeekBar) findViewById(R.id.conscientiousness_cautiousness)).setProgress((int)(children.get(1).getPercentage()*100));
+        ((DiscreteSeekBar) findViewById(R.id.conscientiousness_dutifulness)).setProgress((int)(children.get(2).getPercentage()*100));
+        ((DiscreteSeekBar) findViewById(R.id.conscientiousness_imagination)).setProgress((int)(children.get(3).getPercentage()*100));
+        ((DiscreteSeekBar) findViewById(R.id.conscientiousness_self_discipline)).setProgress((int)(children.get(4).getPercentage()*100));
+    }
+    private void initExtraversion(Trait openinness)
+    {
+        ((DiscreteSeekBar) findViewById(R.id.intr_extr)).setProgress((int)(openinness.getPercentage()*100));
+        List<Trait> children=openinness.getChildren();
+        ((DiscreteSeekBar) findViewById(R.id.intr_extr_activity_level)).setProgress((int)(children.get(0).getPercentage()*100));
+        ((DiscreteSeekBar) findViewById(R.id.intr_extr_assertiveness)).setProgress((int)(children.get(1).getPercentage()*100));
+        ((DiscreteSeekBar) findViewById(R.id.intr_extr_cheerfulness)).setProgress((int)(children.get(2).getPercentage()*100));
+        ((DiscreteSeekBar) findViewById(R.id.intr_extr_excitement_seeking)).setProgress((int)(children.get(3).getPercentage()*100));
+        ((DiscreteSeekBar) findViewById(R.id.intr_extr_gregariousness)).setProgress((int)(children.get(5).getPercentage()*100));
+    }
+    private void initAgreeabl(Trait openinness)
+    {
+        ((DiscreteSeekBar) findViewById(R.id.agreeabl)).setProgress((int)(openinness.getPercentage()*100));
+        List<Trait> children=openinness.getChildren();
+        ((DiscreteSeekBar) findViewById(R.id.agreeabl_altruism)).setProgress((int)(children.get(0).getPercentage()*100));
+        ((DiscreteSeekBar) findViewById(R.id.agreeabl_cooperation)).setProgress((int)(children.get(1).getPercentage()*100));
+        ((DiscreteSeekBar) findViewById(R.id.agreeabl_modesty)).setProgress((int)(children.get(2).getPercentage()*100));
+        ((DiscreteSeekBar) findViewById(R.id.agreeabl_sympath)).setProgress((int)(children.get(4).getPercentage()*100));
+        ((DiscreteSeekBar) findViewById(R.id.agreeabl_trust)).setProgress((int)(children.get(5).getPercentage()*100));
+    }
+
 }
